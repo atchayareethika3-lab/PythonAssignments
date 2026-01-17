c430c480b173d2d8f210cb721eb4b01b02665458
atchayareethika3-lab <atchayareethika3@gmail.com>
2026-01-18 00:22:22 +0530

    Assignment_1 added

--- Task_1_Perform_Basic_Mathematical_Operations.py
+++ Task_1_Perform_Basic_Mathematical_Operations.py
@@ -1,19 +1,12 @@
 number_1=float(input("Enter a number:"))
 number_2=float(input("Enter another number:"))
-operation= str(input('Enter the operation to perform:add/sub/mul/div'))
 
 add=number_1 + number_2
 sub=number_1 - number_2
 mul=number_1 * number_2
 div=number_1 / number_2
 
-if operation == 'add':
-    print(add)
-elif operation == 'sub':
-    print(sub)
-elif operation == 'mul':
-    print(mul)
-elif operation == 'div':
-    print(div)
-else :
-    print('invalid input ')
+print('Addtion =',add)
+print('Subtraction =',sub)
+print('Multiplication =',mul)
+print('Division =',round(div,2))
