483808ff2f367f00dfcb07bd4dfa37da23cfa54c
atchayareethika3-lab <atchayareethika3@gmail.com>
2026-01-18 21:23:24 +0530

    task 1 added

--- Task_1_Perform_Basic_Mathematical_Operations.py
+++ Task_1_Perform_Basic_Mathematical_Operations.py
@@ -6,7 +6,7 @@ sub=number_1 - number_2
 mul=number_1 * number_2
 div=number_1 / number_2
 
-print('Addtion =',add)
+print('Addition =',add)
 print('Subtraction =',sub)
 print('Multiplication =',mul)
 print('Division =',round(div,2))
